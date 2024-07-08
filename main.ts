ec878adc4109ba6ea852fbf628e40016f9a15278
Muhammad Hassan Khan <gonha9725@gmail.com>
2024-07-08 18:58:14 +0500

    Name Cases
    Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase .

---
+++ main.ts
@@ -0,0 +1,15 @@
+      //  Store the person's name in a variable
+
+let myName = "hassan khan "
+
+       // Print the name in uppercase
+
+ myName = myName.toUpperCase();
+         console.log(myName);
+
+         //  Print the name in lowercase
+
+         myName = myName.toLowerCase();
+                 console.log(myName);
+
+       
